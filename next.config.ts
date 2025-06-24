6e8154e9a635dc69bc75b463724de1bb17330a28
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-06-24 14:40:02 +0900

    next.config.ts에 API 리다이렉트 기능 추가: /api/:path* 경로를 localhost:4000으로 리다이렉트하도록 설정

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
